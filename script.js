c1539ff491d8c0174cc74086e95fe124f5ee07f1
Andrew <74207235+Andrew1705@users.noreply.github.com>
2021-07-28 16:22:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var Raza cilindrului (r) = document.querySelector ("#Raza cilindrului")
+var H = document.querySelector ("#Inaltume (H)")
+var button = document.querySelector ("#calculateBTN")
+
+var Razacilindrului:= document.querySelector ("#Raza cilindrului (π)")
+
+console.log(calculateBTN)
